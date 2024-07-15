d9e7c7da07ae7105ea83d86f945e71cbaf232b0d
ChamodMullegama <chamod.mullegama@gmail.com>
2024-07-15 22:58:46 +0530

    know alignment

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(myapp());
@@ -9,43 +10,52 @@ class myapp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       home: SafeArea(
         child: Scaffold(
-          // body: Column(
+          body: Container(
+            width: 300,
+            height: 300,
+            padding: const EdgeInsets.all(10),
+            margin: const EdgeInsets.all(10),
+            color: Colors.amberAccent,
+            child: const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "flutter is easy to create",
+                  style: TextStyle(
+                    fontSize: 40,
+                  ),
+                ),
+                Text(
+                  "dart is easy to create",
+                  style: TextStyle(fontSize: 40),
+                ),
+                Icon(
+                  Icons.ac_unit,
+                  size: 40,
+                )
+              ],
+            ),
+          ),
+
+          // body: Row(
           //   children: [
           //     Text(
-          //       "flutter is easy to create",
+          //       "flutter is easy",
           //       style: TextStyle(
-          //         fontSize: 40,
-          //       ),
-          //     ),
-          //     Text(
-          //       "dart is easy to create",
-          //       style: TextStyle(fontSize: 40),
+          //           fontSize: 50,
+          //           fontFamily: AutofillHints.addressCity,
+          //           color: Colors.black),
           //     ),
           //     Icon(
-          //       Icons.ac_unit,
-          //       size: 40,
+          //       Icons.abc_rounded,
+          //       size: 50,
           //     )
           //   ],
           // ),
-
-          body: Row(
-            children: [
-              Text(
-                "flutter is easy",
-                style: TextStyle(
-                    fontSize: 50,
-                    fontFamily: AutofillHints.addressCity,
-                    color: Colors.black),
-              ),
-              Icon(
-                Icons.abc_rounded,
-                size: 50,
-              )
-            ],
-          ),
         ),
       ),
     );
