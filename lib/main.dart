86e6c56e5a8351254881d8addb1a7334e9dbe3ae
Chamod Mullegama <149089781+ChamodMullegama@users.noreply.github.com>
2024-07-18 15:57:28 +0530

    add 4 layout

--- lib/main.dart
+++ lib/main.dart
@@ -171,18 +171,142 @@
 
 // 3 app layout
 
-import 'dart:nativewrappers/_internal/vm/lib/ffi_native_type_patch.dart';
+// import 'dart:nativewrappers/_internal/vm/lib/ffi_native_type_patch.dart';
+
+// import 'package:flutter/material.dart';
+// import 'package:flutter/widgets.dart';
+
+// void main() {
+//   runApp(myapp());
+// }
+
+// class myapp extends StatelessWidget {
+//   const myapp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: "flutter app",
+//       home: Scaffold(
+//         appBar: AppBar(
+//           backgroundColor: Color(0xff9E00FF),
+//         ),
+//         body: Padding(
+//           padding: EdgeInsets.symmetric(horizontal: 20),
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//             children: [
+//               Row(
+//                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                 children: [
+//                   Container(
+//                     width: 150,
+//                     height: 150,
+//                     decoration: BoxDecoration(
+//                       borderRadius: BorderRadius.circular(10),
+//                       color: Colors.red,
+//                     ),
+//                   ),
+//                   Container(
+//                     width: 150,
+//                     height: 150,
+//                     decoration: BoxDecoration(
+//                       borderRadius: BorderRadius.circular(10),
+//                       color: Colors.red,
+//                     ),
+//                   )
+//                 ],
+//               ),
+//               Column(
+//                 children: [
+//                   Container(
+//                     width: double.infinity,
+//                     height: 150,
+//                     color: Colors.blue,
+//                   ),
+//                 ],
+//               ),
+//               SizedBox(
+//                 height: 300,
+//                 child: Row(
+//                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                   children: [
+//                     Column(
+//                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                       children: [
+//                         Row(
+//                           children: [
+//                             Container(
+//                               width: 75,
+//                               height: 75,
+//                               decoration: const BoxDecoration(
+//                                 color: Color.fromARGB(255, 222, 33, 243),
+//                               ),
+//                             ),
+//                             Container(
+//                               width: 75,
+//                               height: 75,
+//                               decoration: const BoxDecoration(
+//                                 color: Color.fromARGB(255, 222, 33, 243),
+//                               ),
+//                             )
+//                           ],
+//                         )
+//                       ],
+//                     ),
+//                     Column(
+//                       children: [
+//                         Row(
+//                           children: [
+//                             Container(
+//                               width: 75,
+//                               height: 75,
+//                               decoration: const BoxDecoration(
+//                                 color: Colors.blue,
+//                               ),
+//                             ),
+//                             Container(
+//                               width: 75,
+//                               height: 75,
+//                               decoration: const BoxDecoration(
+//                                 color: Colors.yellow,
+//                               ),
+//                             ),
+//                             Container(
+//                               width: 75,
+//                               height: 75,
+//                               decoration: const BoxDecoration(
+//                                 color: Colors.blue,
+//                               ),
+//                             )
+//                           ],
+//                         )
+//                       ],
+//                     )
+//                   ],
+//                 ),
+//               )
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// layout 4
 
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(myapp());
+  runApp(const myapp());
 }
 
 class myapp extends StatelessWidget {
   const myapp({super.key});
 
+  final double BorderRadiussc = 100;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -190,103 +314,138 @@ class myapp extends StatelessWidget {
       title: "flutter app",
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Color(0xff9E00FF),
+          backgroundColor: const Color(0xff9E00FF),
         ),
         body: Padding(
           padding: EdgeInsets.symmetric(horizontal: 20),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    width: 150,
-                    height: 150,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.red,
-                    ),
-                  ),
-                  Container(
-                    width: 150,
-                    height: 150,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.red,
-                    ),
-                  )
-                ],
-              ),
-              Column(
-                children: [
-                  Container(
-                    width: double.infinity,
-                    height: 150,
-                    color: Colors.blue,
-                  ),
-                ],
-              ),
               SizedBox(
-                height: 300,
+                width: double.infinity,
+                height: 200,
                 child: Row(
-                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Column(
-                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          children: [
-                            Container(
-                              width: 75,
-                              height: 75,
-                              decoration: const BoxDecoration(
-                                color: Color.fromARGB(255, 222, 33, 243),
-                              ),
-                            ),
-                            Container(
-                              width: 75,
-                              height: 75,
-                              decoration: const BoxDecoration(
-                                color: Color.fromARGB(255, 222, 33, 243),
-                              ),
-                            )
-                          ],
-                        )
-                      ],
+                    Container(
+                      width: 150,
+                      height: 150,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.red,
+                      ),
                     ),
                     Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Row(
-                          children: [
-                            Container(
-                              width: 75,
-                              height: 75,
-                              decoration: const BoxDecoration(
-                                color: Colors.blue,
-                              ),
-                            ),
-                            Container(
-                              width: 75,
-                              height: 75,
-                              decoration: const BoxDecoration(
-                                color: Colors.yellow,
-                              ),
-                            ),
-                            Container(
-                              width: 75,
-                              height: 75,
-                              decoration: const BoxDecoration(
-                                color: Colors.blue,
-                              ),
-                            )
-                          ],
-                        )
+                        Container(
+                          width: 150,
+                          height: 150,
+                          color: Colors.red,
+                        ),
+                        Container(
+                          width: 150,
+                          height: 150,
+                          color: const Color.fromARGB(255, 244, 241, 54),
+                        ),
                       ],
                     )
                   ],
                 ),
-              )
+              ),
+              Container(
+                width: double.infinity,
+                height: 200,
+                color: const Color.fromARGB(158, 255, 247, 0),
+              ),
+              Container(
+                width: double.infinity,
+                height: 200,
+                color: const Color.fromARGB(158, 255, 132, 0),
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: 200,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      width: 150,
+                      height: 150,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.blue,
+                      ),
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          width: 150,
+                          height: 150,
+                          color: const Color.fromARGB(255, 33, 243, 226),
+                        ),
+                        Container(
+                          width: 150,
+                          height: 150,
+                          color: const Color.fromARGB(255, 255, 0, 0),
+                        ),
+                        Container(
+                          width: 150,
+                          height: 150,
+                          color: Color.fromARGB(255, 0, 255, 221),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: 200,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      width: 150,
+                      height: 150,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(BorderRadiussc),
+                        color: Colors.green,
+                      ),
+                    ),
+                    Container(
+                      width: 150,
+                      height: 150,
+                      color: Colors.green,
+                    ),
+                    Container(
+                      width: 150,
+                      height: 150,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(BorderRadiussc),
+                        color: Colors.green,
+                      ),
+                    ),
+                    Container(
+                      width: 150,
+                      height: 150,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(BorderRadiussc),
+                        color: Colors.green,
+                      ),
+                    ),
+                    Container(
+                      width: 150,
+                      height: 150,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(BorderRadiussc),
+                        color: Colors.green,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
