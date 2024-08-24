15aa5e4d9babece5c01d084c99d80a05235b7731
Chamod Mullegama <149089781+ChamodMullegama@users.noreply.github.com>
2024-08-24 20:36:40 +0530

    Update home_page.dart

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -24,7 +24,7 @@ class HomePage extends StatelessWidget {
         child: Column(
           children: [
             Text(
-              "user interface with flutter flutter  ngfdnhgfd hgdfh bdgrfh        ",
+              "user interface with flutter flutter",
               style: TextStyle(
                 fontSize: 24,
                 color: Colors.white,
