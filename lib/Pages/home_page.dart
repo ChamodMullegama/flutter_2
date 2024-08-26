6e26d768fc8f7255ee461df635feb532229f19a4
Chamod Mullegama <149089781+ChamodMullegama@users.noreply.github.com>
2024-08-26 11:56:27 +0530

    Update home_page.dart

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -9,7 +9,7 @@ class HomePage extends StatelessWidget {
       backgroundColor: Colors.black,
       appBar: AppBar(
         title: Text(
-          "My app dffffff  ",
+          "My app",
           style: TextStyle(color: Colors.white, fontWeight: FontWeight.w300),
         ),
         backgroundColor: Colors.pinkAccent,
