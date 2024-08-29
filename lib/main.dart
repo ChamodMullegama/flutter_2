1ec584a9fc5036430529d3a25ba9980d3c16b872
Chamod Mullegama <149089781+ChamodMullegama@users.noreply.github.com>
2024-08-29 21:36:57 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@
 //               crossAxisAlignment: CrossAxisAlignment.start,
 //               children: [
 //                 Text(
-                  "flutter is easy to create ghdhgf   ",
+                  "flutter is easy to create",
 //                   style: TextStyle(
 //                     fontSize: 40,
 //                   ),
